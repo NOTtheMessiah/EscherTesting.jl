de66f04ac24fd248e52e7d464a17258746ffe8ca
Brian Cohen <brian.cohen.88@gmail.com>
2015-08-13 03:54:26 -0400

    Add initial tests and update readme

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # EscherTesting.jl
 Selenium Test scripts and Utilities for Escher.jl
+
+
+##### Requirements 
+
+* [Selenium Server Standalone](http://seleniumhq.org/)
+* [Python binding for Selenium Remote Control](http://pypi.python.org/pypi/selenium)
+* Web Browser with a WebDriver implementation (defaults to [Chromium](http://www.chromium.org/))
+
+##### Project structure
+
+for each  ```$(JULIADIR)/example/<name>.jl``` there should be an associated ```test_<name>.py```.  Each contains independent tests cases 
+Initially, tests will be in Python and Chrome-only, but will be ported to either use PyCall or call WebDriver directly within Julia, and will be cross-platform (in that you can choose which browser to run the tests in from the command line).
+
+##### Cross-platform tests
+
+For use of Firefox or Safari (or any other supported browser), replace ```driver = webdriver.Chrome()``` with the respective webdriver. As of now, Firefox has limited support for WebComponents, as the polyfills are not complete. 
+
+# Test Cases
+Each test file will perform independent tests via the python unittest module.
+
+### Completed
+* form.jl 
+  - Input name and check
+  - Input rating and check
+* layout.jl
+  - Check if the first vertical flow is square (height == width)
+### Future
+* minesweeper.jl
+  - inject game board state via JS, and check clicking
+* tex.jl
+  - Check to see if it outputs TeX
+
+# External References
+
+##### Escher
+
+* https://shashi.github.io/Escher.jl/reactive.html
+* https://github.com/shashi/Patchwork.jl
+* https://github.com/JuliaLang/Reactive.jl
+
+##### Selenium
+
+* https://seleniumhq.github.io/selenium/docs/api/py/index.html
+* https://selenium-python.readthedocs.org/en/latest/index.html
+* https://github.com/SeleniumHQ/selenium/tree/master/py
+
+##### Polymer
+
+* https://www.polymer-project.org/0.5/docs/start/getting-the-code.html
+* https://github.com/Polymer/web-component-testre
+
+##### Shadow DOM
+
+* introduction: http://www.html5rocks.com/en/tutorials/webcomponents/shadowdom/
+* proposal: https://w3c.github.io/webcomponents/spec/shadow/
+
+
---
+++ test_form.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
+
+
+class Form(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(30)
+        self.base_url = "http://localhost:5555"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+    
+    def test_rate(self):
+        driver = self.driver
+        driver.get(self.base_url + "/form.jl")
+
+        for i in range(60):
+            try:
+                if self.is_element_present(By.CSS_SELECTOR, "div.flow.vertical  > span"): break
+            except: pass
+            time.sleep(1)
+        else: self.fail("time out")
+        time.sleep(1)
+        try: self.assertEqual("Dict{Any,Any}()", driver.find_element_by_css_selector("div.flow.vertical  > span").text)
+        except AssertionError as e: self.verificationErrors.append(str(e))
+
+        time.sleep(1)
+
+        # need to replace this with mouse events instead of directly editing the Polymer properties
+        myslider = driver.find_element_by_tag_name("paper-slider")
+        driver.execute_script("document.getElementsByTagName('paper-slider')[0].value=5")
+        driver.execute_script("document.getElementsByTagName('paper-slider')[0].fire('change')") # doesn't work in firefox
+
+        driver.find_element_by_tag_name("paper-button").click()
+        time.sleep(1)
+        try: self.assertEqual("Dict{Any,Any}(:submit=>Escher.LeftButton(),:name=>\"\",:_trigger=>:submit,:rating=>5)", driver.find_element_by_css_selector("div.flow.vertical  > span").text)
+        except AssertionError as e: self.verificationErrors.append(str(e))
+
+    def test_name(self):
+        driver = self.driver
+        driver.get(self.base_url + "/form.jl")
+
+
+        for i in range(60):
+            try:
+                if self.is_element_present(By.CSS_SELECTOR, "div.flow.vertical  > span"): break
+            except: pass
+            time.sleep(1)
+        else: self.fail("time out")
+        time.sleep(1)
+        try: self.assertEqual("Dict{Any,Any}()", driver.find_element_by_css_selector("div.flow.vertical  > span").text)
+        except AssertionError as e: self.verificationErrors.append(str(e))
+
+        myname = driver.find_element_by_id("name")
+        myname.clear()
+        myname.send_keys("John Doe")
+        time.sleep(1)
+
+        driver.find_element_by_tag_name("paper-button").click()
+        time.sleep(1)
+        try: self.assertEqual("Dict{Any,Any}(:submit=>Escher.LeftButton(),:name=>\"John Doe\",:_trigger=>:submit,:rating=>1)", driver.find_element_by_css_selector("div.flow.vertical  > span").text)
+        except AssertionError as e: self.verificationErrors.append(str(e))
+
+    
+    def is_element_present(self, how, what):
+        try: self.driver.find_element(by=how, value=what)
+        except NoSuchElementException, e: return False
+        return True
+    
+    def is_alert_present(self):
+        try: self.driver.switch_to_alert()
+        except NoAlertPresentException, e: return False
+        return True
+    
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally: self.accept_next_alert = True
+    
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+if __name__ == "__main__":
+    unittest.main()
